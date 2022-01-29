175f72a6701f0adec3df3675eaabceb9c18d35e0
softwinner813 <68693499+softwinner813@users.noreply.github.com>
2022-01-28 19:59:08 -0800

    update front page display.blade.php

--- Modules/FeaturedProducts/Resources/views/frontpagedisplay.blade.php
+++ Modules/FeaturedProducts/Resources/views/frontpagedisplay.blade.php
@@ -4,9 +4,5 @@
     </div>
 </div>
 <div class="row">
-    @foreach($featuredProducts as $product)
-        <div class="col-md-4 my-md-0 my-2 col-12">
-            @include('includes.product.card', ['product' => $product])
-        </div>
-    @endforeach
+    
 </div>
