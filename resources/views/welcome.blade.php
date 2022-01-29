cce2aaeaf3cdb215b005bc7c114a1228b9064fee
softwinner813 <68693499+softwinner813@users.noreply.github.com>
2022-01-28 20:26:18 -0800

    welcome.blade.php fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,77 +61,7 @@
                 @include('featuredproducts::frontpagedisplay')
             @endisModuleEnabled
 
-            <div class="row mt-4">
-
-                <div class="col-md-4">
-                    <h4>
-                        Top Vendors
-                    </h4>
-                    <hr>
-                    @foreach(\App\Vendor::topVendors() as $vendor)
-                        <table class="table table-borderless table-hover">
-                            <tr>
-                                <td>
-                                    <a href="{{route('vendor.show',$vendor)}}"
-                                       style="text-decoration: none; color:#212529">{{$vendor->user->username}}</a>
-                                </td>
-                                <td class="text-right">
-                                    <span class="btn btn-sm @if($vendor->vendor->experience >= 0) btn-primary @else btn-danger @endif active"
-                                          style="cursor:default">Level {{$vendor->getLevel()}}</span>
-
-                                </td>
-                            </tr>
-                        </table>
-                    @endforeach
-                </div>
-                <div class="col-md-4">
-                    <h4>
-                        Latest orders
-                    </h4>
-                    <hr>
-                    @foreach(\App\Purchase::latestOrders() as $order)
-                        <table class="table table-borderless table-hover">
-                            <tr>
-                                <td>
-                                    <img class="img-fluid" height="23px" width="23px"
-                                         src="{{ asset('storage/'  . $order->offer->product->frontImage()->image) }}"
-                                         alt="{{ $order->offer->product->name }}">
-                                </td>
-                                <td>
-                                    {{str_limit($order->offer->product->name,50,'...')}}
-                                </td>
-                                <td class="text-right">
-                                    {{$order->getSumLocalCurrency()}} {{$order->getLocalSymbol()}}
-                                </td>
-                            </tr>
-                        </table>
-                    @endforeach
-                </div>
-
-                <div class="col-md-4">
-                    <h4>
-                        Rising vendors
-                    </h4>
-                    <hr>
-                    @foreach(\App\Vendor::risingVendors() as $vendor)
-                        <table class="table table-borderless table-hover">
-                            <tr>
-                                <td>
-                                    <a href="{{route('vendor.show',$vendor)}}"
-                                       style="text-decoration: none; color:#212529">{{$vendor->user->username}}</a>
-                                </td>
-                                <td class="text-right">
-                                    <span class="btn btn-sm @if($vendor->vendor->experience >= 0) btn-primary @else btn-danger @endif active"
-                                          style="cursor:default">Level {{$vendor->getLevel()}}</span>
-                                </td>
-                            </tr>
-                        </table>
-                    @endforeach
-                </div>
-
-
-            </div>
-
+         
 
         </div>
 
